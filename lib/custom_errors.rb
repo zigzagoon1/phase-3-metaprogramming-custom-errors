22881a3b914a9a160a7fbd5cb2470ae8acc88215
KellyMark <kmark602@gmail.com>
2023-04-27 11:49:18 -0400

    commit tree a27a66a18d3c0308b9ecb08b406b33f4eafb8f93

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,10 +6,23 @@ class Person
   end
 
   def get_married(person)
+    if person.is_a?(Person)
     self.partner = person
     person.partner = self
+    else
+      begin
+        raise PartnerError
+        rescue PartnerError => error
+          puts error.message
+        end
+    end
   end
-
+  
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class."
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
